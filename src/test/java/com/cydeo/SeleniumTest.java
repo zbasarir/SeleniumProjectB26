a96a876e7b474e1422477a7c484b00c015d0eae0
zbasarir <ziynetyapar@gmail.com>
2022-05-08 11:19:56 -0400

    Initial commit

---
+++ src/test/java/com/cydeo/SeleniumTest.java
@@ -0,0 +1,19 @@
+package com.cydeo;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumTest {
+    public static void main(String[] args) {
+        //Step1: Setting up the web driver manager
+
+        WebDriverManager.chromedriver().setup();
+
+        //Step2: Create instance of the chrome driver
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.google.com");
+    }
+}
