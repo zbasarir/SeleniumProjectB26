ccfe7d532b51c87288f4423963f7106d7d5dc29a
zbasarir <ziynetyapar@gmail.com>
2022-05-08 14:58:04 -0400

    Task1 is added

--- src/test/java/com/cydeo/test/day1_selenium_intro/Basic_Navigations.java
+++ src/test/java/com/cydeo/test/day1_selenium_intro/Basic_Navigations.java
@@ -12,6 +12,14 @@ public class Basic_Navigations {
         // 2- First create instance of WebDriver
         WebDriver driver = new ChromeDriver();
 
+        Thread.sleep(3000);
+
+        // 3- Make our page full screen (it will cover the all window)
+        driver.manage().window().maximize();
+       // driver.manage().window().fullscreen();
+
+
+
         //Second create -> get method:
         driver.get("http://www.etsy.com");
 
@@ -22,10 +30,67 @@ public class Basic_Navigations {
 
         //It is using for stopping application. We are giving time to application to see steps
 
-        Thread.sleep(3000);
+        Thread.sleep(3000);// it gets execute the code slowly
 
         //navigate().back() method is for navigating to back from web page
 
         driver.navigate().back(); //open the web show it and then close it!
+
+        Thread.sleep(3000);
+
+        // navigate().forward() method is for navigate to forward from web page
+        driver.navigate().forward();
+
+       // driver.navigate().refresh();method is for refreshing the page
+        driver.navigate().refresh();
+
+
+        //getting title of the current page
+        //driver.getTitle(); will give you the title of the page
+        // get title will give you the title of the page but you have to use println since driver.getTitle(); is a String
+        System.out.println("driver.getTitle() = " + driver.getTitle());
+
+        // getting URL of the web page
+        System.out.println("Etsy page driver.getCurrentUrl() = " + driver.getCurrentUrl());
+
+
+        //Another example:
+
+        driver.get("http://www.amazon.com");
+        System.out.println("Amazon page driver.getTitle() = " + driver.getTitle());
+        System.out.println("Amazon page driver.getCurrentUrl() = " + driver.getCurrentUrl());
+
+        //shortcut for soutv-> finish the code and type .soutv+enter
+        /*On the console we will see:
+         driver.getTitle() = Etsy - Shop for handmade, vintage, custom, and unique gifts for everyone
+        Etsy page driver.getCurrentUrl() = https://www.etsy.com/
+        Amazon page driver.getTitle() = Amazon.com. Spend less. Smile more.
+        Amazon page driver.getCurrentUrl() = https://www.amazon.com/
+         */
+
+
+        //driver.close(); this will close the currently open page
+        driver.close();
+
+       // driver.quit(); this will close whole current open apps
+        driver.quit();
+
+
+
+        //String currentTitle = driver.getTitle();
+        String currentTitle = driver.getTitle();
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
---
+++ src/test/java/com/cydeo/test/day1_selenium_intro/Task1_YahooTitleVerification.java
@@ -0,0 +1,38 @@
+package com.cydeo.test.day1_selenium_intro;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Task1_YahooTitleVerification {
+
+    /*
+    TC #1: Yahoo Title Verification
+        1. Open Chrome browser
+        2. Go to https://www.yahoo.com
+        3. Verify title:
+                Expected: Yahoo
+     */
+
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.get("htpps://www.yahoo.com");
+
+        // actual title comes from selenium
+        String actualTitle = driver.getTitle();
+
+        // expected title comes from requirement
+        String expectedTitle = "Yahoo";
+
+        if(actualTitle.contains(expectedTitle)){
+            System.out.println("Title verification is passed!");
+        }else {
+            System.out.println("Title verification is failed!");
+        }
+
+        driver.quit();
+    }
+}
